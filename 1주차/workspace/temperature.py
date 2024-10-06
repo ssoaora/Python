a847c0cff802c7edc1894b419759d5edff3212b8
Dongmin Lee <ssoaora@gmail.com>
2024-10-07 03:25:46 +0900

    🔖 Week 1

---
+++ 1주차/workspace/temperature.py
@@ -0,0 +1,8 @@
+print("This program changes Celsius to Fahrenheit")
+print("Please enter the temperature in Celsius")
+
+celsius = input()  # get the temperature in Celsius
+fahrenheit = (float(celsius) * 1.8) + 32  # convert to Fahrenheit
+
+print("The temperature in Celsius is:", celsius)
+print("The temperature in Fahrenheit is:", fahrenheit)
